694d933984c446d19ef63170d1926ee88469cb13
Patapkin Nikita <patapkin15@yandex.ru>
2022-07-07 11:27:05 +0300

    feat: add Enum Status

--- src/main/java/ru/practicum/shareit/booking/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/BookingMapper.java
@@ -1,15 +1,4 @@
 package ru.practicum.shareit.booking;
 
-import ru.practicum.shareit.booking.dto.BookingDto;
-
 public class BookingMapper {
-
-    public static BookingDto toBookingDto(Booking booking) {
-        return new BookingDto(
-                booking.getStartDateTime(),
-                booking.getEndDateTime(),
-                new BookingDto.Item(booking.getItem().getId(), booking.getItem().getName()),
-                booking.isStatus()
-        );
-    }
 }
