ada782413b0d9d0ca9845aef4c3a16bf6c83b6cd
Patapkin Nikita <patapkin15@yandex.ru>
2022-08-26 00:20:38 +0300

    fix : delete dependence validation in gateway

--- server/src/main/java/ru/practicum/server/item/dto/OwnerItemDto.java
+++ server/src/main/java/ru/practicum/server/item/dto/OwnerItemDto.java
@@ -5,8 +5,6 @@ import lombok.Data;
 import lombok.Getter;
 import ru.practicum.server.comment.CommentDto;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.Collection;
 
 /**
@@ -17,24 +15,12 @@ import java.util.Collection;
 public class OwnerItemDto {
 
     private final Long id;
-
-    @NotBlank(message = "Имя не может быть пустым.")
-    @NotNull(message = "Имя должно передаваться параметром.")
     private String name;
-
-    @NotBlank(message = "Описание не может быть пустым.")
-    @NotNull(message = "Описание должно передаваться параметром.")
     private String description;
-
-    @NotNull(message = "Нужно указать доступна вещь или нет.")
     private Boolean available;
-
     private Long requestId;
-
     private Collection<CommentDto> comments;
-
     private Booking lastBooking;
-
     private Booking nextBooking;
 
     @AllArgsConstructor
